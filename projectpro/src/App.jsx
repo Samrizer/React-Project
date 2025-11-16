e15418b1a3434440b8e78422c5f071e41503db59
Samrizer <bijoybaishnab42@gmail.com>
2025-11-17 01:53:56 +0600

    React-Project done

---
+++ projectpro/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    
+
+    <>
+    
+    
+
+ <div id = 'parent'>
+      <h1 id = 'child1'>Hello</h1>
+      <h2 id = 'child2'> Hey </h2>
+    </div>
+
+    <div id = "Bro"> </div>
+
+    
+    </>
+  )
+}
+
+export default App
+
+
+
